93024486760ce96cd801f335b9697dfc8e71213a
Mohamed Shamim <173870949+Mohamed-Shamim@users.noreply.github.com>
2025-07-22 15:37:08 +0600

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/convert", {
+      const response = await fetch("https://html-to-image-1-ryld.onrender.com/api/convert", {
         method: "POST",
         body: formData,
       });
